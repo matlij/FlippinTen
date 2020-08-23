d8e7598ee7c3eeaa6c8834ecb752a66dc29e9665
Mattias Lij <mattiaslij@gmail.com>
2020-08-23 20:15:52 +0200

    Removed unsused namepsaces

--- FlippinTen/FlippinTen/Views/PickupCardsPage.xaml.cs
+++ FlippinTen/FlippinTen/Views/PickupCardsPage.xaml.cs
@@ -4,11 +4,6 @@ using Rg.Plugins.Popup.Services;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace FlippinTen.Views
